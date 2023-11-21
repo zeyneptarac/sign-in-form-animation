f82d0eb80db315752b5999149971333865892209
zeyneptarac <121973939+zeyneptarac@users.noreply.github.com>
2023-11-21 07:49:07 +0300

    push

---
+++ script.js
@@ -0,0 +1,8 @@
+const signUpButton= document.getElementById("signUp");
+
+const signInButton= document.getElementById("signin'); const container document.getElementById('container");
+
+signUpButton.addEventListener('click', () => { container.classtist.add("right-panel-active"); });
+
+signInButton.addEventListener('click', () => { container.classList.remove("right-panel-active");
+});
